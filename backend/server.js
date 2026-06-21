19501f5518e8c359370003d9897f59ef92a6fe46
yaswanthkumar-07 <234003856+yaswanthkumar-07@users.noreply.github.com>
2026-06-21 21:44:13 +0530

    Add User model schema

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -14,7 +21,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
