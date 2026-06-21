8a70aaed40abd0f3d0a3a02c0ebfefda0c20730b
yaswanthkumar-07 <234003856+yaswanthkumar-07@users.noreply.github.com>
2026-06-21 12:52:20 +0530

    Setup backend dependencies and Express server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    project: "Attendance Management System",
+    status: "Running"
+  });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
