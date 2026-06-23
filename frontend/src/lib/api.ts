d678624d5e22a8c4d2433614409058813b52cc3f
yaswanthkumar-07 <234003856+yaswanthkumar-07@users.noreply.github.com>
2026-06-23 18:21:35 +0530

    production api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://attendance-management-system-we7p.onrender.com/api",
 });
