82f719a4ef2747f7b4c45cc0d7cd1825e32a454e
yaswanthkumar-07 <234003856+yaswanthkumar-07@users.noreply.github.com>
2026-06-24 07:00:59 +0530

    Remove redundant backend folder from frontend

--- frontend/backend/routes/DashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getDashboardStats } = require("../controllers/DashboardController");
-
-const router = express.Router();
-
-router.get("/", getDashboardStats);
-
-module.exports = router;
